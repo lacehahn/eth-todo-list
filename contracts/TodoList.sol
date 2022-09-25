15327bbc452b2ce7973b0e39835112391a142860
Lance <lancehahn@gmail.com>
2022-09-25 20:45:03 +0900

    add the function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,31 @@
-pragma solidity ^0.8.17;
+pragma solidity ^0.5.16;
 
 contract TodoList {
   uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    constructor() public{
+        createTask("Check out dappuniversity.com");
+    }
+  
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+
+
 }
